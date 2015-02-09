eb68ca2cd2a1aa4232ebde444936717cd6a3ea96
Javier Olaechea <pirata@gmail.com>
2015-02-09 05:12:30 -0500

    Remove files from previous idea

--- elblog-install.el
+++
@@ -1,5 +0,0 @@
-;; Script to install elblog
-;;
-;; Main idea
-;;
-;; compute dependencies, download them, concat all of elnode.
--- elblog-startup.el
+++
@@ -1,3 +0,0 @@
-;; Script to startup elblog.
-
-(elblog-publish-dir "~/")
